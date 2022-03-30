d9bf3f6cc716b186b202a1b4036f0f56c3a722c0
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-03-30 13:30:17 -0300

    :sparkles: feat - book modal

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,22 @@ import dotenv from 'dotenv';
 import express from "express";
 import { useRoutes } from './routes';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.Port || 8090;
 
 const app = express();
+
+
+const corsOptions = {
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
 app.use(bodyParser.json());
 // eslint-disable-next-line react-hooks/rules-of-hooks
 useRoutes(app);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
-import React,{ useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+
+import {AiOutlineClose} from 'react-icons/ai';
 
 function App() {
   const [book, setBook] = useState("");
   const [result, setResult] = useState([]);
   const [modalState, setModalState] = useState(false);
   const [selectedBook, setSelectedBook] = useState("");
-  const apiKey = "your api key";
+  const [comments, setComments] = useState([]);
+  const [textAreaComment, setTextAreaComment] = useState("");
+  const [username, setUsername] = useState("");
+
+  const apiKey = "AIzaSyCpUPsTb1dceiCOHxN2URx2vK-sBPywhXw";
 
   function handleChange(event){
     const bookSearch = event.target.value;
@@ -23,29 +29,68 @@ function App() {
   }
 
   function toogleModalClass(){
-    setModalState(!modalState);
+    setModalState(oldState => !oldState);
   }
 
-  function setActiveBlock(item){
+  function setActiveBook(item){
     setSelectedBook(item);
+  }
+
+  useEffect(() => {
+   axios.get("http://localhost:8090/api/comments")
+    .then(data => {
+      const commentsFiltered = data.data.filter(c => c.bookName === selectedBook.volumeInfo.title);
+
+      setComments(commentsFiltered);
+    })
+  }, [selectedBook])
+  
+
+  function handleTextAreaChange(e) {
+    setTextAreaComment(e.target.value);
+  }
+
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
+
+  function handleTextAreaSubmit() {
+    axios.post("http://localhost:8090/api/comments", {
+      personName: username,
+      personComment: textAreaComment,
+      bookName: selectedBook.volumeInfo.title
+    }).then(() => setActiveBook(oldState => oldState))    
   }
 
   return (
     <div className="Container">
       <div className={modalState ? 'modal ativo' : 'modal'}>
-        {selectedBook ? 
+        {selectedBook &&
           <div>
             <h2>{selectedBook.volumeInfo.title}</h2>
             <img
               src={selectedBook.volumeInfo.imageLinks.thumbnail}
               alt={selectedBook.volumeInfo.title}
             />
-            <p>Ano de publicação: {selectedBook.volumeInfo.publishedDate}</p>
-            {selectedBook.volumeInfo.description ? 
-            <p>{selectedBook.volumeInfo.description}</p>
-            : null}
+            {selectedBook.volumeInfo.publishedDate && 
+            <p className='ano-publicacao'><span>Ano de publicação:</span> {selectedBook.volumeInfo.publishedDate}</p>}
+            
+            {selectedBook.volumeInfo.description && 
+            <p className='descricao'><span>Descrição: </span>{selectedBook.volumeInfo.description}</p>
+            }
+            <p className='comentarios'><span>Escrever um comentário:</span></p>
+            <input className='username-input' type="text" placeholder='Seu nome' onChange={handleUsernameChange}  />
+            <textarea placeholder='Seu comentário' onChange={handleTextAreaChange} className='escrever-comentario' />
+            <button className='comentario-button' type="button" onClick={handleTextAreaSubmit}>Publicar</button>
+
+            <p className='comentarios'><span>Comentários:</span></p>
+            {comments && 
+            comments.map((item, index) =>(
+              <p className='comentario' key={index}><span>{item.personName}: </span>{item.personComment}</p>
+            ))}
           </div>
-        : null}
+        }
+        <AiOutlineClose className='icon-close' onClick={() => toogleModalClass()} />
       </div>
       <h1 className="Title">Book search app</h1>
       <form onSubmit={handleSubmit}>
@@ -62,10 +107,10 @@ function App() {
       </form>
       <div className='books'>
       {result.map((item, index) => (
-        item.volumeInfo.imageLinks ?
-        <div key={index} onClick={() => [ setActiveBlock(item)]}>
-          <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.title} />
-        </div> : null
+        item.volumeInfo.imageLinks &&
+        <div key={index} onClick={() => [toogleModalClass(), setActiveBook(item)]}>
+          <img className='book' src={item.volumeInfo.imageLinks.thumbnail} alt={item.title} />
+        </div>
       ))}
       </div>
     </div>
